31386c3b7db1285c70f4414ad5c5d1c78d956f3b
tonyzeng2017 <32418425+tonyzeng2017@users.noreply.github.com>
2017-12-28 10:24:55 +0800

    updated the entrance game.
    updated the entrance game.

--- reduce_png.sh
+++ reduce_png.sh
@@ -1,4 +1,19 @@
 
+defaultSourceDir="./build/jsb-link/frameworks/runtime-src/proj.android-studio"
+
+if [ -n "$1" ] ;then
+    surfix="$1"
+    sourceDir="./build/jsb-link/frameworks/runtime-src/proj.android-$surfix/"
+
+    echo "removing the $defaultSourceDir......"
+    rm -rf $defaultSourceDir
+
+    echo "copy to the $defaultSourceDir......"
+    cp -r $sourceDir $defaultSourceDir
+else
+    echo "use the default source dir~~~~~~~~"
+fi
+
 /Applications/CocosCreator.app/Contents/MacOS/CocosCreator --path ./  --build "platform=android"
 
 #!/bin/bash
@@ -7,7 +22,7 @@ function ergodic(){
         do  
                 if [ -d $1"/"$file ]  
                 then  
-                        ergodic $1"/"$file  
+                    ergodic $1"/"$file  
                 else
                 	filename=$1"/"$file
                 	extension="${filename##*.}"
@@ -16,14 +31,14 @@ function ergodic(){
                 	      ~/tools/pngquant/pngquant -f --quality=65-80 256 $filename -o $filename
                               echo $filename
                 	fi
-                fi  
-        done 
+                fi
+        done
 }
 
 INIT_PATH1="./build/jsb-link/res/raw-assets"
 INIT_PATH2="./build/jsb-link/res/raw-internal"
 
-ergodic $INIT_PATH1
-ergodic $INIT_PATH2
+#ergodic $INIT_PATH1
+#ergodic $INIT_PATH2
 
 /Applications/CocosCreator.app/Contents/MacOS/CocosCreator --path ./  --compile "platform=android"
